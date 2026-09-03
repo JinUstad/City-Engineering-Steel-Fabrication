4fbc9e1773ecb70506bba9f50e4699c34699d7ba
JinUstad <jin.gul0628@gmail.com>
2026-09-03 07:29:55 +0530

    Update bedroom furniture wardrobe image

--- lib/site-data.ts
+++ lib/site-data.ts
@@ -143,9 +143,9 @@ export const SERVICES: Product[] = [
     name: 'Bed Room Furniture',
     category: 'Bedroom Luxury',
     folderName: 'Bed Room Furniture',
-    image: '/product-wardrobe.webp',
+    image: '/product-wardrobe-new.png',
     images: [
-      '/product-wardrobe.webp',
+      '/product-wardrobe-new.png',
       '/product-sofa.webp',
       '/banner-sofa.webp',
       '/product-crockery-unit.webp',
